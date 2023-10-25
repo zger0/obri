0c2242cee1e83dc0618548df2310e0dfd1d5f816
zger0 <127360974+zger0@users.noreply.github.com>
2023-10-26 01:53:36 +0900

    1

--- backend/src/main/java/com/obri/global/WebConfig.java
+++ backend/src/main/java/com/obri/global/WebConfig.java
@@ -1,2 +1,24 @@
-package com.obri.global;public class WebConfig {
+package com.obri.global;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000") // 클라이언트 애플리케이션의 주소
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600);
+    }
 }
